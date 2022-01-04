b9c18eb135bcec404f6d422a2fbb243c8f6d0ebb
kamal14rajput <kamal14rajput@gmail.com>
2022-01-05 00:29:36 +0530

    ADDED: valid last name

--- src/com/bridgelabz/RegexRegistration.java
+++ src/com/bridgelabz/RegexRegistration.java
@@ -6,14 +6,26 @@ import java.util.regex.Pattern;
 public class RegexRegistration {
 
 	public static boolean isFirstName(String firstName) {
-		
+
 		String Regex_FirstName = "^[A-Z]{1}[a-z]{2,}";
 		Pattern pattern = Pattern.compile(Regex_FirstName);
-		
+
 		if (firstName == null) {
 			return false;
 		}
 		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+
+	public static boolean isLastName(String lastName) {
+
+		String Regex_LastName = "^[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(Regex_LastName);
+
+		if (lastName == null) {
+			return false;
+		}
+		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
 }
