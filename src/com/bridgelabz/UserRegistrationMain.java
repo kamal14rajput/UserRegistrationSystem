d5b954c9c7c6771dbd4014dc2dcffd2c58c5462f
kamal14rajput <kamal14rajput@gmail.com>
2022-01-05 00:29:19 +0530

    ADDED: valid last name

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -8,13 +8,22 @@ public class UserRegistrationMain {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the first name:");
 		String firstName = sc.nextLine();
-
+		
 		RegexRegistration regex = new RegexRegistration();
 		if (regex.isFirstName(firstName) == true) {
 			System.out.println("Firstname is Correct");
 		} else {
 			System.out.println("Firstname is Incorrect " + "\n"
 					+ "Enter First name starts with Capital letter and has minimum 3 characters");
+		}
+		
+		System.out.println("Enter the last name:");
+		String lastName = sc.nextLine();
+		if (regex.isLastName(lastName) == true) {
+			System.out.println("Lastname is Correct");
+		} else {
+			System.out.println("Lastname is Incorrect " + "\n"
+					+ "Enter Last name starts with Capital letter and has minimum 3 characters");
 		}
 	}
 
